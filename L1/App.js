15f8e4b34e9f91eea25a052b772bbc8a7b8b308a
nitishchoudhari <nitishchoudhari2904@gmail.com>
2023-12-31 03:06:12 +0530

    ignited-app

--- L1/App.js
+++ L1/App.js
@@ -1,12 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 /* React code*/
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const heading = React.createElement('div', {id : 'parent'}, 
- React.createElement('div', {id : 'child'},
- [React.createElement('h1', {}, 'I am h1 tag'), 
- React.createElement('h2', {}, 'I am h2 tag')])
+const heading = React.createElement('div', { id: 'parent' },
+    React.createElement('div', { id: 'child' },
+        [React.createElement('h1', {}, 'I am h1 tag!!!!!'),
+        React.createElement('h2', {}, 'I am h2 tag')])
 )
 
 root.render(heading)
