78e1c2696b86c0fbcca4d1cc2094b94bf37a547f
nitishchoudhari <nitishchoudhari2904@gmail.com>
2024-01-01 01:53:34 +0530

    package json scripts and react comp-elem composition

--- L1/App.js
+++ L1/App.js
@@ -4,14 +4,10 @@ import ReactDOM from "react-dom/client";
 /* React code*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const heading = React.createElement('div', { id: 'parent' },
-    React.createElement('div', { id: 'child' },
-        [React.createElement('h1', {}, 'I am h1 tag!!!!!'),
-        React.createElement('h2', {}, 'I am h2 tag')])
-)
 
-root.render(heading)
+const elem = <h1>Hey I am react element</h1>
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// const heading = React.createElement('h1', {}, 'Hello world');
-// root.render(heading);
+const Title = () => <h2>{elem} Functional component 2 title</h2>
+const Heading = () => <h1> {Title()} Hello Functional Component</h1>;
+
+root.render(<Heading />);
